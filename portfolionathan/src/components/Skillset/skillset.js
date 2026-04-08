dd41cdeaa217001f5966c2b9d2cedebd7cdf7d28
Sainth Nathan Ahoussi <E129941@adgroup.michelin.com>
2026-04-08 16:59:44 +0200

    Amélioration section Soft Skils( Ajout Font Style, Texte, Ligne Barrière) -> travailler responsivité Mobile et Animation Hover

--- portfolionathan/src/components/Skillset/skillset.js
+++ portfolionathan/src/components/Skillset/skillset.js
@@ -4,31 +4,38 @@ import './skillset.css';
 const Skillset = () => {
     return (
         <section className="skillset">
-            <h4>Skillset</h4>
+            <h4>Soft Skills</h4>
             <p>The following skills reflect the expertise and know-how developed throughout my professional and academic journey, covering various areas essential to the success of my missions.</p>
             <div className="skills-container">
                 <div className="column">
-                    <p>Persévérance</p>
-                    <p>Esprit Critique</p>
-                    <p>Curiosité</p>
-                    <p>Autonomie</p>
-                    <p>Pensée Analytique</p>
+                    <h5>Cognitive Skills</h5>
+                    <p>Critical Thinking</p>
+                    <p>Curiosity</p>
+                    <p>Analytical Thinking / Problem Solving</p>
+                    <p>Decision Making</p>
+                    <p>Strong Comprehension</p>
+                    <p>Organized</p>
+                    <p>Attention to Detail / Rigor</p>
                 </div>
-                <div className="divider"></div>
                 <div className="column">
-                    <p>Esprit d'équipe</p>
-                    <p>Adaptabilité</p>
+                    <h5>Interpersonal Skills</h5>
+                    <p>Teamwork</p>
                     <p>Communication</p>
-                    <p>Prise de décision</p>
-                    <p>Ordonné</p>
+                    <p>Active Listening</p>
+                    <p>Conflict Management</p>
+                    <p>Technical Collaboration</p>
+                    <p>Technical Vulgarization </p>
+                    <p>Constructive Feedback</p>
                 </div>
-                <div className="divider"></div>
-                <div className="column">
-                    <p>Polyvalence</p>
-                    <p>Forte Tenacité</p>
-                    <p>Bonne Compréhension</p>
-                    <p>Rigueur</p>
-                    <p>Multitâche</p>
+                <div className="column_bis">
+                    <h5>Personal / Behavioral Skills</h5>
+                    <p>Perseverance</p>
+                    <p>Autonomy</p>
+                    <p>Adaptability</p>
+                    <p>Strong Tenacity</p>
+                    <p>Multitasking</p>
+                    <p>Stress Management</p>
+                    <p>Technology Monitoring</p>
                 </div>
             </div>
         </section>
