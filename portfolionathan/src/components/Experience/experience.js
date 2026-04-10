0b6d7634d945099a482f6e5d142910068fd103aa
Sainth Nathan Ahoussi <E129941@adgroup.michelin.com>
2026-04-10 17:03:01 +0200

    Fin de la mise à jour de la section Experience, Nouveau design frise ainsi que informations

--- portfolionathan/src/components/Experience/experience.js
+++ portfolionathan/src/components/Experience/experience.js
@@ -5,59 +5,40 @@ import 'aos/dist/aos.css';
 
 const experiences = [
     {
-        title: "Stage - Infotech Mayotte",
-        date: "Janvier - Mars 2019 ",
+        title: "Internship - IT SIRH Employee Experience Analyst and Developpment.",
+        date: "February - Mars 2026 ",
+        Entreprise:"Michelin (Clermont-Ferrand)",
+        Logo:"https://media.licdn.com/dms/image/v2/D4D0BAQEZ8x7ezlOdEw/company-logo_200_200/company-logo_200_200/0/1707743589225/fondation_dentreprise_michelin_logo?e=2147483647&v=beta&t=XgzSKNeW6JOUNOSNnLAy2YiqHv-Nk7qrtLQbUmMEXKM",
         description: [
-            "- Accueil et vente au client",
-            "- Prise des commandes et des rendez-vous",
-            "- Organisation des dossiers"
+            "- Analysis and presentation of the R1 2026 release for the Workday Michelin teams and country stakeholders.",
+            "- Development of a multilingual Power Apps decision-support tool, integrated with Microsoft Lists and deployed on Microsoft SharePoint for Michelin SP and C&B teams.",
+            "- Participation in Innovation Labs focused on the creation of an AI Agent using Flowise and Extend, including the delivery of a Proof of Concept (POC).",
+            "- React development of a widget displaying Workday training content within an Employee Space."
         ]
     },
     {
-        title: "SECRÉTAIRE & ANIMATEUR - ASSOCIATION BB PLAYERS (VIRY-CHÂTILLON)",
-        date: "Juin - Juillet 2021",
+        title: "Internship - Web Developer ",
+        date: "June - August 2024",
+        Entreprise: "SINGA (Paris)",
+        Logo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDLCJrtK6jgBouHotuLNlkBjTWfz5LYXT8NA&s",
         description: [
-            "- Animation de groupe d'enfants (7-14 ans)",
-            "- Détection de talent",
-            "- Mission de secrétariat"
+            "- Diagnosis and prototyping for the Maison des Réfugiés new website",
+            "- Inventory and update of Singa’s IT equipment and digital tools",
+            "- Creation of a Notion directory website to showcase incubated projects",
+            "- Development and automation of a program to generate folder tree structures for files stored in Google Drive",
+            "- Analysis, wireframing, and development of a V1 version of the website for the Voix en Exil program"
         ]
     },
     {
-        title: " CAISSIER POLYVALENT - Somaco Mayotte",
-        date: "Mars - Juin 2022",
+        title: "Internship - IT Technician Assistant",
+        date: "January - Mars 2019 ",
+        Entreprise:"INFOTECH (Mayotte)",
+        Logo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRN3OBiLsREjjnF6gFpYv2ECDUKkie0Aghy4w&s",
         description: [
-            "- Encaissement",
-            "- Mise en rayon",
-            "- Préparation des commandes"
-        ]
-    },
-    {
-        title: "PRÉPARATEUR DE COMMANDES  - SODIFRAM HYPER DISCOUNT",
-        date: "Juin - Août  2022",
-        description: [
-            "- Préparation et tri des produits pour expédition",
-            "- Respect des délais et des consignes de sécurité",
-            "- Organisation et gestion des stocks"
-        ]
-    },
-    {
-        title: "PRÉPARATEUR DE COMMANDES - AUCHAN RETAIL",
-        date: "Juillet - Septembre 2023",
-        description: [
-            "- Préparation et tri des produits pour expédition",
-            "- Respect des délais et des consignes de sécurité",
-            "- Organisation et gestion des stocks"
-        ]
-    },
-    {
-        title: "Stage - Singa Paris",
-        date: "Juin - Août 2024",
-        description: [
-            "- Diagnostic et prototypage du site web de la Maison des Réfugiés",
-            "- Inventaire du matériel et des outils informatiques de Singa",
-            "- Création d'un répertoire Notion pour les projets incubés",
-            "- Développement et automatisation d’un programme de génération de structure arborescente",
-            "- Analyse et programmation d’une version V1 pour le site web du programme Voix en Exil"
+            "- Customer reception and sales of IT services",
+            "- Handling of orders and appointment scheduling",
+            "- Organization and management of client files",
+            "- Assistance with the repair of IT equipment"
         ]
     }
 ];
@@ -68,7 +49,8 @@ const Experience = () => {
 
     return (
         <section id="experience-section" className="experience-timeline">
-            <h2>Expériences Professionnelles</h2>
+            <h2>IT Professional Experiences</h2>
+            <h5>This section presents all the internships and apprenticeships I have completed throughout my IT career.</h5>
             <div className="timeline">
                 {experiences.map((exp, index) => (
                     <div 
@@ -77,8 +59,13 @@ const Experience = () => {
                         data-aos="fade-up"
                     >
                         <div className="timeline-content">
-                            <span className="date">{exp.date}</span>
+                             <span>
+                                <img src={exp.Logo} width="50" height="50"/>
+                            </span>
                             <h3>{exp.title}</h3>
+                            <h4>{exp.Entreprise}</h4>
+                            <span className="date">{exp.date}</span>
+                            <br />
                             <br />
                             <ul>
                                 {exp.description.map((desc, i) => (
