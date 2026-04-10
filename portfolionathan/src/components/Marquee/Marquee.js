af7faccbaccefa756089ee51020461c8c137bfad
Sainth Nathan Ahoussi <E129941@adgroup.michelin.com>
2026-04-10 11:20:37 +0200

    Amélioration Marquee (Vitesse et Texte)

--- portfolionathan/src/components/Marquee/Marquee.js
+++ portfolionathan/src/components/Marquee/Marquee.js
@@ -5,7 +5,7 @@ const Marquee = () => {
   return (
     <div className="marquee-container">
       <div className="marquee-text">
-          Urgently Seeking a 2‑Year Work‑Study Program in Artificial Intelligence or Cybersecurity in Paris
+          Urgently Seeking a 2 to 3‑Year Apprenticeship in Artificial Intelligence or Cybersecurity in Paris or the Suburbs of Paris to finance my studies and gain real experiences !!
       </div>
     </div>
   );
