5573063c7eb9b4c786dba750c2db0f126c7aebb9
Sainth Nathan Ahoussi <E129941@adgroup.michelin.com>
2026-04-10 14:51:35 +0200

    Fin mise à jour Ordre et Font Section Languages

--- portfolionathan/src/components/Langues/langues.js
+++ portfolionathan/src/components/Langues/langues.js
@@ -14,22 +14,19 @@ import CongoImage from '../../Images/Languages/congo.png';
 
 const Langues = () => {
     return (
-        <div class="languages">
+        <div className="languages">
             <h4>Languages</h4>
-            <p>I’m someone who love to learn new things especially if it’s something that 
-            <br />will be really useful to me.</p>
-                <div class="grilleLanguages">
-                <div class="japon"><img src= {JaponImage} /> <p> In Progress</p></div>
-                <div class="espagne"><img src= {EspagneImage} /> <p> Intermediate (B1)</p></div>
-                <div class="portugal"><img src= {PortugalImage} /> <p> In Progress</p></div>
-                <div class="coteIvoire"><img src= {CIVImage} /> <p> In Progress</p></div>
-                <div class="anglais"><img src= {AnglaisImage} /> <p><a href="https://www.etsglobal.org/fr/en/digital-score-report/9C35571E742355627BC2843B03ADAE18D0BBBFED3FB9FC622AC205225CAC9F1CVGxWU0wwancrSW1mVFE1OElzclk4Rkh6S0RLejk0UG40djYrMVQyNmFoTTRRb2JK?utm_campaign=sharing_dsr&utm_medium=qr-code-click&utm_source=dsr">Intermediate (B2- TOEIC Link)</a></p></div>
-                <div class="france"><img src= {FranceImage} /> <p>Natal</p></div>
-                <div class="coree"><img src= {CoreeImage} /> <p>In Progress</p></div>
-                <div class="nigeria"><img src= {NigeriaImage} /> <p>In Progress</p></div>
-                <div class="creole"><img src= {CreoleImage} /> <p>In Progress</p></div>
-                <div class="italie"><img src= {ItalieImage} /> <p>In Progress</p></div>
-                <div class="congo"><img src= {CongoImage} /> <p>In Progress</p></div>
+            <h5>I’m someone who love to learn new things especially if it’s something that will be really useful to me.</h5>
+                <div className="grilleLanguages">
+                <div className="france"><img src= {FranceImage} /> <p>Natal</p></div>
+                <div className="anglais"><img src= {AnglaisImage} /> <p><a href="https://www.etsglobal.org/fr/en/digital-score-report/9C35571E742355627BC2843B03ADAE18D0BBBFED3FB9FC622AC205225CAC9F1CVGxWU0wwancrSW1mVFE1OElzclk4Rkh6S0RLejk0UG40djYrMVQyNmFoTTRRb2JK?utm_campaign=sharing_dsr&utm_medium=qr-code-click&utm_source=dsr">Intermediate (B2- TOEIC Link)</a></p></div>
+                <div className="espagne"><img src= {EspagneImage} /> <p> Intermediate (B1)</p></div>
+                <div className="portugal"><img src= {PortugalImage} /> <p> In Progress</p></div>
+                <div className="japon"><img src= {JaponImage} /> <p> In Progress</p></div>
+                <div className="coteIvoire"><img src= {CIVImage} /> <p> In Progress</p></div>
+                <div className="creole"><img src= {CreoleImage} /> <p>In Progress</p></div>
+                <div className="coree"><img src= {CoreeImage} /> <p>In Progress</p></div>  
+                <div className="congo"><img src= {CongoImage} /> <p>In Progress</p></div>
             </div>
       </div>
     );
