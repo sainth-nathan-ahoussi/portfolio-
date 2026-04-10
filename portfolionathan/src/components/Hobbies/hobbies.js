0c327a8ab5084ca2acf00a3e443177848590520e
Sainth Nathan Ahoussi <E129941@adgroup.michelin.com>
2026-04-10 15:04:34 +0200

    Fin de modification section Hobbies

--- portfolionathan/src/components/Hobbies/hobbies.js
+++ portfolionathan/src/components/Hobbies/hobbies.js
@@ -17,21 +17,20 @@ const Hobbies = () => {
     return (
         <div class="hobbies">
         <h4>Hobbies</h4>
-        <p>Things that matter to me outside of school, these are interests that I pursue for some, and for others, they are actual activities that I engage in.
-        </p>
+        <h5>Things that matter to me outside of school, these are interests that I pursue for some, and for others, they are actual activities that I engage in.</h5>
         <div class="grilleHobbies">
-                <div class="Basket"><img src={BasketImage} /></div>
-                <div class="Boxe"><img src={BoxeImage} /></div>
-                <div class="Foot"><img src={FootImage} /></div>
-                <div class="Photo"><img src={PhotoImage} /></div>
-                <div class="Montage"><img src={MontageImage} /></div>
-                <div class="Langues"><img src={LanguesImage} /></div>
-                <div class="Piano"><img src={PianoImage} /></div>
-                <div class="Guitare"><img src={GuitareImage} /></div>
-                <div class="Musique"><img src={Musiquemage} /></div>
-                <div class="Voyager"><img src={VoyageImage} /></div>
-                <div class="Jeu"><img src={JeuImage} /></div>
-                <div class="Electronique"><img src={ElectroniqueImage} /></div>
+                <div class="Basket"><img src={BasketImage} /> <p>Basketball</p></div>
+                <div class="Boxe"><img src={BoxeImage} /> <p>Boxe</p></div>
+                <div class="Foot"><img src={FootImage} /> <p>Football</p></div>
+                <div class="Photo"><img src={PhotoImage} /> <p>Shooting and Filming</p></div>
+                <div class="Montage"><img src={MontageImage} /> <p>Editing</p></div>
+                <div class="Langues"><img src={LanguesImage} /> <p>Languages</p></div>
+                <div class="Piano"><img src={PianoImage} /> <p>Piano</p></div>
+                <div class="Guitare"><img src={GuitareImage} /> <p>Guitar</p></div>
+                <div class="Musique"><img src={Musiquemage} /> <p>Music</p></div>
+                <div class="Voyager"><img src={VoyageImage} /> <p>Travel</p></div>
+                <div class="Jeu"><img src={JeuImage} /> <p>Videos Games</p></div>
+                <div class="Electronique"><img src={ElectroniqueImage} /> <p>Electronic</p></div>
                 </div>
         </div>
     );
